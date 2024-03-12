3b8fd86e320bae0c02fcf3428fac8191cdb4e8d5
Quoc Khanh <23730095@ms.uit.edu.vn>
2024-03-12 11:36:36 +0700

    first commit

---
+++ src/main/java/com/khanhnq/identity/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.khanhnq.identity.controller;
+
+import com.khanhnq.identity.dto.request.UserCreationRequest;
+import com.khanhnq.identity.dto.request.UserUpdateRequest;
+import com.khanhnq.identity.entity.UserTable;
+import com.khanhnq.identity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping()
+    UserTable createUser(@RequestBody UserCreationRequest request) {
+        return userService.createUser(request);
+    }
+
+    @GetMapping()
+    List<UserTable> getUserList() {
+        return userService.getUserList();
+    }
+
+    @GetMapping("/{userId}")
+    UserTable getUserById(@PathVariable("userId") String userId) {
+        return userService.getUserById(userId);
+    }
+
+    @PutMapping("/{userId}")
+    UserTable updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable String userId) {
+        userService.deleteUser(userId);
+        return "Delete user success";
+    }
+}
